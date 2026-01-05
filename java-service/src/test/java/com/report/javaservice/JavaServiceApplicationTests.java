e22c17fac5dcaef86d790832b77ad0c8e4ed1db4
tomasbecher <tbecher@lamansys.com.ar>
2026-01-05 08:44:21 -0300

    Java service setup

---
+++ java-service/src/test/java/com/report/javaservice/JavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.report.javaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
